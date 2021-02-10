323e001d1d9360bcfd3f88d766e4a45d84507247
terranadams <terran.adams7225@stu.mtec.edu>
2021-02-10 11:41:44 -0700

    found another bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db') // This isn't something we install, it's something we write.
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 
 app.use(express.urlencoded({extended: false}))
