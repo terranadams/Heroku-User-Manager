09b953460165a20062e2f31139a4e42710045ab7
terranadams <terran.adams7225@stu.mtec.edu>
2021-02-10 11:33:33 -0700

    got db to connect

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const app = express()
+const db = require('./db') // This isn't something we install, it's something we write.
 const port = process.env.port || 3000
-console.log(`App name = ${process.env.APP_NAME}`)
-console.log(`Title = ${process.env.title}`)
+
 
 app.use(express.urlencoded({extended: false}))
 
 //API for the client (browser)
-app.get('/users', (req, res) => {
-    res.send('All the users')
-})
+app.get('/', db.getUsers)
 
-app.post('/createUser', (req, res => {
-    res.send('New user created')
-}))
+app.post('/createUser', db.createUser)
 
 app.listen(port, () => {
-    console.log(`App listening on port ${ort}`)
+    console.log(`App listening on port ${port}`)
 })
--- db.js
+++ db.js
@@ -1,1 +1,53 @@
 // API to the database
+const Pool = require('pg').Pool
+const url = require('url')
+const DBConnectionString = process.env.DATABASE_URL // this gets provided from heroku or .env
+
+const params = url.parse(DBConnectionString)
+
+const auth = params.auth.split(':')
+let SSL = process.env.SSL
+if (SSL === 'false') {
+    SSL = false
+} else if (SSL = 'heroku') {
+   SSL = {rejectUnauthorized: false}
+} else {
+    SSL = true
+}
+
+const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: SSL
+}
+
+const pool = new Pool(config)
+
+const getUsers = (req, res) => { // This gets put into the callback for the initial get request
+    let getUsersSQL = 'select * from users '
+    pool.query(getUsersSQL, (err, results) => {
+        if(err) throw err
+        // console.log(results)
+        res.status(200).json(results.rows)
+    })
+}
+
+const createUser = (req, res) => {
+    // console.log(`db getUsers`);
+    const email = req.body.email
+    let getUsersSQL = `insert into users (first, last, email, age) values ($1, $2, $3, $4);`
+    pool.query(getUsersSQL, [first, last, email, age] (err, results) => {
+        if (err) throw err
+    //   console.log(results)
+      res.status(200).json(results.rows);
+    });
+  };
+
+module.exports = {
+    getUsers,
+    createUser
+}
+
